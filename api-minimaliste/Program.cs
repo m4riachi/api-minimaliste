a559064a3ba03bd3683a2ff9705fd10a83081eeb
taoufiq <taoufiq.benmessaoud@gmail.com>
2024-11-17 11:48:53 +0100

    Update code formatting and environment variables
    Aligned code formatting in Program.cs for readability. Added DOTNET_SYSTEM_GLOBALIZATION_INVARIANT environment variable to launchSettings.json. Updated vcs.xml to include Git directory mapping.

--- api-minimaliste/Program.cs
+++ api-minimaliste/Program.cs
@@ -22,20 +22,19 @@ var summaries = new[]
 };
 
 app.MapGet("/weatherforecast", () =>
-{
-    var forecast =  Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast")
-.WithOpenApi();
-
+    {
+        var forecast = Enumerable.Range(1, 5).Select(index =>
+                new WeatherForecast
+                (
+                    DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+                    Random.Shared.Next(-20, 55),
+                    summaries[Random.Shared.Next(summaries.Length)]
+                ))
+            .ToArray();
+        return forecast;
+    })
+    .WithName("GetWeatherForecast")
+    .WithOpenApi();
 app.Run();
 
 record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
